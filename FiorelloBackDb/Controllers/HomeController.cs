38439e71f7b1899158a8e783b75e59104a1a85ec
Tahmin Fatiyev <tahmin.fatiyev@gmail.com>
2022-03-09 22:24:05 +0400

    Add project files.

---
+++ FiorelloBackDb/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using FiorelloBackDb.Data;
+using FiorelloBackDb.Models;
+using FiorelloBackDb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiorelloBackDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+            SliderDetail detail = await _context.SliderDetails.FirstOrDefaultAsync();
+            HomeVM homeVM = new HomeVM
+            {
+                Sliders = sliders,
+                Detail = detail
+            };
+            return View(homeVM);
+        }
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FiorelloBackDb/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FiorelloBackDb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiorelloBackDb.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<SliderDetail> SliderDetails { get; set; }
+    }
+}
